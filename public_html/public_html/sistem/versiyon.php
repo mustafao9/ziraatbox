c59aa087406b294fb6bc79e4a5fcdde5bf3b5372
Mustafa Satılmış <satilmisenator@gmail.com>
2026-09-22 00:09:41 +0300

    Proje dosyalari dogrudan kok dizine yerlestirildi

--- public_html/public_html/sistem/versiyon.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * ZiraatBox - Sistem Versiyon Yapılandırması
- */
-if (!defined('SISTEM_VERSIYON')) {
-    define('SISTEM_VERSIYON', '1.0.2');
-}
--- public_html/public_html/sistem/yedek_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ZiraatBox Otomatik Yedekleme Yapılandırması
-// Son Güncelleme: 2026-03-16 03:33:35
-
-define('YEDEK_SIKLIK', 'gunluk');
-define('YEDEK_ICERIK', 'full');
-?>
